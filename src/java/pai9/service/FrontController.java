ba32ec35efcdb82d66b73dfb0f72b53832d881a5
borovsky <fl.borovsky@gmail.com>
2014-12-04 16:06:55 +0100

    zrobiona labka 9

--- src/java/pai9/service/FrontController.java
+++ src/java/pai9/service/FrontController.java
@@ -12,6 +12,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Request;
+import pai9.app.MainDispatcher;
+import pai9.app.core.IView;
+import pai9.app.view.MainView;
 
 /**
  * REST Web Service
@@ -30,12 +34,16 @@ public class FrontController {
      */
     public FrontController() {
         System.out.println("dzialasdsdsa");
+        md = new MainDispatcher();
     }
+    private MainDispatcher md;
 
     @GET
     @Path("/route/{cmd}")
     @Consumes("text/html")
     public String process(@PathParam("cmd") String cmd) {
-        return cmd + " : routed";
+        IView selectedView = md.delegate(context);
+        String content = selectedView.renderView(context);
+        return content;
     }
 }
